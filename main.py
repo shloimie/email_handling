98f78a3e42ae32bd547ed18359d210eef888302c
shloimie <33160133+shloimie@users.noreply.github.com>
2022-01-09 18:07:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import email_handling as eh
+
+eh.start_send()
+eh.send_main(send_to="autointernet910@gmail.com", message="this is a test of non-class module")
+eh.send_main(send_to="autointernet910@gmail.com", message="this is email number 2 in a shot")
+eh.end_send()
+
+my_inbox = eh.get_inbox()
+for thing in my_inbox:
+    from_ = thing["from"]
+    body = thing["body"].strip()
+    print(from_ + body)
